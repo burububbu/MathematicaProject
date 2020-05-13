1843e9fb2bcdba3a31abfd2893f178df291cfa37
a.giacche <alessandrogiacche@gmail.com>
2020-05-13 13:00:02 +0200

    amo mathematica

--- package.m
+++ package.m
@@ -1,92 +1,76 @@
 (* ::Package:: *)
 
-(* :Title: AngleCircBackend*)
+(* :Title: Angoli al Centro e alla Circonferenza *)                         
 (* :Context: AngleCircBackend`*)
-(* :Author: Don't Panic!*)
-(* :Summary: package for radiantsAngle and circumference exercises *)
-(* :Copyright: Don't Panic! 2020*)
-(* :Package Version:1*)
-(* :Mathematica Version:12*)
-(* :History:*)
-(* :Keywords: *)
-(* :Sources:biblio*)
-(* :Discussion:*)
+(* :Author: Boccuto, Celozzi, Giacch\[EGrave] *)
+(* :Summary: Package per la definizione e la risoluzione della classe di esercizi riguardo angoli al centro e alla circonferenza.
+	La classe di esercizi presa in esame riguarda il calcolo dell'area e del perimetro di due triangoli in una circonferenza con
+	due vertici in comune. Uno inscritto e l'altro con il terzo vertice in "O". *)
+(* :Copyright: Don't Panic! 2020 *)
+(* :Package Version: 1.0 *)
+(* :Mathematica Version: 12.1 *)
+(* :Keywords: cerchio, circonferenza, centro, angolo, angoli, triangolo, triangoli,
+				circle, circumference, center, angle, angles, triangle, triangles *)
+(* :Sources:
+	https://reference.wolfram.com/,
+	https://community.wolfram.com/,
+	https://mathematica.stackexchange.com/ *)
 
-circleAngleGraphicsElements::usage = "circleAngleGraphicsElements[] "
-Begin["Private`"]
-cAGE[type_, radiantsAngle_, choice_] := Module[{circleCenter, alpha, AAngle, circAngle, points, beta, choices},
-If[And[Mod[radiantsAngle, Pi/(type*1.)]==0,choice==1], Return[{Text["NO"]}]];
-circleCenter={0,0};
-alpha=type*Which[resto==0, Pi/(type*1.), True, resto]/.resto -> Mod[radiantsAngle, Pi/(type*1.)];
-(*alpha=Which[type\[Equal]1, Which[resto\[Equal]0, Pi, True, resto]/.resto \[Rule] Mod[radiantsAngle, Pi], type\[Equal]2, 2*Which[resto==0, Pi/2., True, resto]/.resto -> Mod[radiantsAngle, Pi/2.]];*)
-(*alpha=Which[type==1, Which[radiantsAngle>Pi,(2*Pi-radiantsAngle),True,radiantsAngle], type==2, 2*Which[resto==0, Pi/2, True, resto]/.resto -> Mod[radiantsAngle, Pi/2]];*)
+BeginPackage["AngleCircBackend`"]
 
-AAngle=RandomReal[{0,Pi}];
-circAngle=RandomReal[{alpha+0.1,2*Pi-0.1}];
-points={CirclePoints[{1,AAngle},1][[1]]};
-AppendTo[points,RotationMatrix[alpha].points[[1]]];
-beta=Pi-alpha;
+circleAngleGraphicsElements::usage = "circleAngleGraphicsElements[type_, angle_, choice_] ritorna una lista di oggetti grafici contenente:
+- La circonferenza goniometrica su cui vengono costruiti i triangoli
+- Due ToolTips che mostrano le dimensioni dell'angolo al centro e di quello alla circonferenza, rispettivamente
+- Il triangolo AOB dove O \[EGrave] il centro della circonferenza e A e B formano un angolo al centro che rispetta i criteri passati come parametri
+- Il triangolo ABC inscritto nella circonferenza che insiste sullo stesso arco AB e rispetta i criteri passati come parametri.
+Parametri:	
+- \"type\" definisce il tipo di angolo che \"angle\" rappresenta: 1\[Rule]angolo al centro, 2\[Rule]angolo alla circonferenza
+- \"angle\" rappresenta la grandezza dell'angolo specificato
+- \"choice\" specifica se il centro della circonferenza deve essere interno o esterno al triangolo \"ABC\": 1\[Rule]esterno , 2\[Rule]interno.
+NOTA: Se i parametri non sono coerenti (es: [1,180,1]) verr\[AGrave] ritornata una lista con un unico elemento che rappresenta un messaggio di testo che specifica l'impossibilit\[AGrave] di seguire le condizioni imposte.
+"
 
-(*TODO Unevaluated*)
-choices={RandomChoice[{-1, 1}]*(alpha/2 + 0.1 + RandomReal[{0, beta  - 0.1}]), alpha/2 + beta + RandomReal[{0, alpha}]};
-AppendTo[points,RotationMatrix[choices[[choice]]].middlePoint/.middlePoint -> RotationMatrix[alpha/2].points[[1]]];
+Begin["`Private`"]
 
-{
-Circle[circleCenter, 1] ,
-Point[circleCenter],
-Point /@ points,
+circleAngleGraphicsElements[type_, angle_, choice_] := Module[{alpha, aAngle, cAngle, points, choices,radiantsAngle},
+	radiantsAngle=angle*Degree;
+	If[Mod[radiantsAngle, Pi/(type*1.)] == 0 && choice == 1, Return[{Text["Impossibile disegnare."]}]];
+	alpha=type*Which[resto == 0, Pi/(type*1.), True, resto]/.resto ->Mod[radiantsAngle,Pi/(type*1.)];
+	aAngle=RandomReal[{0,Pi}];
 
-{Opacity[0.3],Style[Triangle[{points[[1]],points[[2]],circleCenter}], Blue]},
-{Opacity[0.3],Style[Triangle[points], Red]},
+	points = {A, RotationMatrix[alpha].A }/.A->CirclePoints[{1, aAngle}, 1][[1]];
+	
+	cAngle=Which[
+		choice==1 (* esterno *),
+		(RandomChoice[{-1, 1}]*RandomReal[{alpha+0.2,-alpha+2*Pi}])/2,
+		choice==2 (* interno *),
+		RandomReal[{-alpha,alpha}]/2+Pi
+	];
+	AppendTo[points, RotationMatrix[cAngle].middlePoint/.middlePoint->RotationMatrix[alpha/2].points[[1]]];
 
-Line[Append[points,  points[[1]]]],(* triangolo con angolo al centro *)
-Line[{points[[1]],points[[2]],circleCenter, points[[1]]}], (* triangolo con angolo alla circonferenza *)
+	Return[
+		{
+			Circle[circleCenter, 1] ,
+			Point[circleCenter],
+			Point/@points,
 
-Tooltip[Style[Circle[circleCenter, 0.2, {AAngle, AAngle + alpha}], Red, Thick ], ToString[NumberForm[alpha/Degree,{3,2}]]<>"\[Degree]"],
-Tooltip[Style[Circle[points[[3]], 0.2, {angletemp, angletemp+alpha/2}], Blue, Thick ], ToString[NumberForm[alpha/(2.*Degree),{3,2}]]<>"\[Degree]"],
-(* Annotation[Style[Circle[points[[3]], 0.2, {angletemp, angletemp+alpha/2}], Blue, Thick ], alpha/2, "Mouse"], *)
+			{
+				Opacity[0.3],
+				Blue,
+				Triangle[{points[[1]], points[[2]], circleCenter}],
+				Red, Triangle[points]
+			},
 
-MapIndexed[ Text[FromCharacterCode[#2[[1]]+ 64] , #1, {1.5,-1.5}]&, points], 
-Text["O", circleCenter,{1.5,-1.5}]
-}/.angletemp -> PlanarAngle[points[[3]] -> {{points[[3]][[1]] + 1, points[[3]][[2]]}, points[[1]]}]
+			Line[Append[points,  points[[1]]]], (* triangolo con angolo al centro *)
+			Line[{points[[1]], points[[2]], circleCenter, points[[1]]}], (* triangolo con angolo alla circonferenza *)
+
+			Tooltip[{Red, Thick, Circle[circleCenter, 0.2, {aAngle, aAngle + alpha}]}, StringJoin[ToString@NumberForm[alpha/Degree, {3, 2}],"\[Degree]"]],
+			Tooltip[{Blue, Thick, Circle[points[[3]], 0.2, {angletemp, angletemp + alpha/2}]}, StringJoin[ToString@NumberForm[alpha/(2.*Degree), {3, 2}],"\[Degree]"]],
+
+			MapIndexed[Text[FromCharacterCode[#2[[1]] + 64] , #1, offset]&, points], 
+			Text["O", circleCenter,offset]
+		}/.{angletemp -> PlanarAngle[points[[3]] -> {{points[[3]][[1]] + 1, points[[3]][[2]]}, points[[1]]}],circleCenter->{0,0}, offset->{1.5,-1.5}}
+	]
 ]
-
-circleAngleGraphicsElements[type_, angle_, choice_] := Module[{radiantsAngle},
-radiantsAngle=angle*Degree;
-cAGE[type, radiantsAngle, choice]
-]
-
-(*circleAngleGraphicsElements[type_, angle_, choice_] := Module[{circleCenter, alpha, AAngle, circAngle, points, beta, choices, radiantsAngle},
-circleCenter={0,0};
-radiantsAngle=angle*Degree;
-alpha=Which[radiantsAngle>Pi,(2*Pi-radiantsAngle),True,radiantsAngle];
-AAngle=RandomReal[{0,Pi}];
-circAngle=RandomReal[{alpha+0.1,2*Pi-0.1}];
-points={CirclePoints[{1,AAngle},1][[1]]};
-AppendTo[points,RotationMatrix[alpha].points[[1]]];
-beta=Pi-alpha;
-
-(*TODO Unevaluated*)
-choices={ RandomChoice[{-1, 1}] *(alpha/2 + 0.1+RandomReal[{0, beta  - 0.1}]),alpha/2 + beta +RandomReal[{0, alpha}]};
-AppendTo[points,RotationMatrix[choices[[choice]]].middlePoint/.middlePoint \[Rule] RotationMatrix[alpha/2].points[[1]]];
-
-{
-Circle[circleCenter, 1] ,
-Point[circleCenter],
-Point /@ points,
-
-{Opacity[0.3],Style[Triangle[{points[[1]],points[[2]],circleCenter}], Blue]},
-{Opacity[0.3],Style[Triangle[points], Red]},
-
-Line[Append[points,  points[[1]]]],(* triangolo con angolo al centro *)
-Line[{points[[1]],points[[2]],circleCenter, points[[1]]}], (* triangolo con angolo alla circonferenza *)
-
-Tooltip[Style[Circle[circleCenter, 0.2, {AAngle, AAngle + alpha}], Red, Thick ], ToString[NumberForm[alpha/Degree,{3,2}]]<>"\[Degree]"],
-Tooltip[Style[Circle[points[[3]], 0.2, {angletemp, angletemp+alpha/2}], Blue, Thick ], ToString[NumberForm[alpha/(2.*Degree),{3,2}]]<>"\[Degree]"],
-(* Annotation[Style[Circle[points[[3]], 0.2, {angletemp, angletemp+alpha/2}], Blue, Thick ], alpha/2, "Mouse"], *)
-
-MapIndexed[ Text[FromCharacterCode[#2[[1]]+ 64] , #1, {1.5,-1.5}]&, points], 
-Text["O", circleCenter,{1.5,-1.5}]
-}/.angletemp \[Rule] PlanarAngle[points[[3]] \[Rule] {{points[[3]][[1]] + 1, points[[3]][[2]]}, points[[1]]}]
-]*)
 End[]
+EndPackage[]
